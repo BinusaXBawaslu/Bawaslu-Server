fdbf60b357f505a92421688c079802170d389060
alfnbntng <alfanbintang28@gmail.com>
2024-01-19 11:02:32 +0700

    memperbaiki database dialect123

--- src/main/java/com/Binusa/BawasluServer/service/IsiInformasiKeteranganService.java
+++ src/main/java/com/Binusa/BawasluServer/service/IsiInformasiKeteranganService.java
@@ -34,7 +34,6 @@ public class IsiInformasiKeteranganService {
 
     public IsiInformasiKeteranganApiResponseDTO save(IsiInformasiKeteranganDTO isiInformasiKeteranganDTO) {
         IsiInformasiKeterangan isiInformasiKeterangan = new IsiInformasiKeterangan();
-        isiInformasiKeterangan.setId(1L);
         isiInformasiKeterangan.setDokumen(isiInformasiKeteranganDTO.getDokumen());
 
         JenisKeterangan jenisKeterangan = jenisKeteranganRepository.findById(isiInformasiKeteranganDTO.getJenisKeteranganId())
